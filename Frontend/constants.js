8b9787f420d2374a5458c35d8bd4ff7c5db56554
Om Kariya <161995427+odk1572@users.noreply.github.com>
2025-01-27 03:15:10 +0530

    Update constants.js

--- Frontend/constants.js
+++ Frontend/constants.js
@@ -1,4 +1,4 @@
 export const NODE_ENV = '' //development
 
-export const BASE_API = NODE_ENV === 'development' ? "http://localhost:4000" : "https://mern-foodapp-1.onrender.com"
-export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://mern-food-app-one.vercel.app/"
+export const BASE_API = NODE_ENV === 'development' ? "http://localhost:4000" : "https://mealmate-p16n.onrender.com"
+export const STRIPE_REDIRECT_URL = NODE_ENV === 'development' ? "http://localhost:5173" : "https://mealmate-p16n.onrender.com"
